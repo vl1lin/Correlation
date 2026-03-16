6c9d72d0626e948e0d41813177cfa50c85d4889b
vl1lin <v1adbarten@mail.ru>
2026-03-16 21:56:16 +0300

    Начало работы над реализацией классов для чтения файлов

---
+++ import_data.py
@@ -0,0 +1,23 @@
+import json
+import numpy as np
+import pandas as pd
+import random
+from abc import ABC, abstractmethod
+from typing import Dict, Optional, Any
+
+class DataTransport:
+
+
+    def get_data_from_json(self):
+        pass
+
+    def get_data_from_csv(self):
+        pass
+
+    def create_random_data(self):
+        pass
+
+    def import_created_data(self):
+        pass
+
+    def get_data_from
