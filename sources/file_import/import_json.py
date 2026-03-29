ca962b7cf9040a65c248c4ec25e80852aa9db3cc
vl1lin <v1adbarten@mail.ru>
2026-03-29 17:41:19 +0300

    Сделал стратегию чтения json файлов

--- sources/file_import/import_json.py
+++ sources/file_import/import_json.py
@@ -1,13 +1,28 @@
 import logging
 from interface import FileStrategy
-from typing import Any, Union
+from typing import Any
 from pathlib import Path
+import json
 
 
 logger = logging.getLogger(__name__)
 
 
 class JsonStrategy(FileStrategy):
-    def file_read(self, *, file_path: Union[Path, str]) -> Any:
-        logger.info(f"Загрузка данных: {file_path}")
+    def _file_read(self, *, file_path: str) -> Any:
+        path = Path(file_path)
 
+        logger.info(f"Проверка существования файла")
+        if not path.exists():
+            logger.error(f"Файл: {file_path} не найден")
+            raise FileNotFoundError(f"Файл: {file_path} не найден")
+
+        logger.info(f"Загрузка данных: {file_path}")
+        with open(file_path, "r", encoding='utf-8') as f:
+            try:
+                data = json.load(f)
+                logger.info(f"Данные успешно загружены в формате: {type(data)}")
+            except json.JSONDecodeError as e:
+                logger.error(f"Ошибка формата JSON: {e}")
+                raise ValueError(f"Ошибка формата JSON: {e}")
+        return data
--- sources/file_import/interface.py
+++ sources/file_import/interface.py
@@ -1,10 +1,24 @@
 from abc import ABC, abstractmethod
-from typing import Any, Union
-from pathlib import Path
+from typing import Any
+import pandas as pd
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class FileStrategy(ABC):
     """Стратегия чтения файлов"""
     @abstractmethod
-    def file_read(self, *, file_path: Union[Path, str]) -> Any:
+    def _file_read(self, *, file_path: str) -> Any:
         pass
+
+    def create_storage(self, *, file_path: str) -> pd.DataFrame:
+        logger.info(f"Получение данных методом _file_read")
+        data = self._file_read(file_path=file_path)
+
+        logger.info(f"Конвертация данных в DataFrame")
+        if isinstance(data, list):
+            return pd.json_normalize(data)
+        else:
+            return pd.DataFrame(data)
