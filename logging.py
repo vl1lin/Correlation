9df93fbe44e1386299a1d0dcdec00278a7c06531
vl1lin <v1adbarten@mail.ru>
2026-03-29 16:28:38 +0300

    Создал файл с настройками логгера

--- logging.py
+++ logging.py
@@ -1,11 +1,10 @@
 import logging
 import sys
-from datetime import datetime
 
 
 def setup_logger(name, log_file, level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s — %(name)s — %(levelname)s — %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
+                                  datefmt='%Y-%m-%d %H:%M:%S')
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -13,10 +12,13 @@ def setup_logger(name, log_file, level=logging.INFO):
     file_handler = logging.FileHandler(log_file, encoding='utf-8')
     file_handler.setFormatter(formatter)
 
-    stream_heandler = logging.StreamHandler(sys.stdout)
-    stream_heandler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    logger.addHandler(stream_heandler)
+    logger.addHandler(stream_handler)
 
     return logger
+
+
+log = setup_logger('CorrelationApp', "app.log")
